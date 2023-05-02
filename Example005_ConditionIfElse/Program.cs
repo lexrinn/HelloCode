f9dd8898eab284f13840783ae1e63b5b4cdd33a1
LexRinn <lexrinn@outlook.com>
2023-05-02 16:45:12 +0500

    add Example005 Condition If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "alex")
+{
+    Console.WriteLine("Yoohooo, it's ALEX!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
