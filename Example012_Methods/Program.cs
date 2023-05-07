02271627205c0401e46f9e8ddaac0c5852ccd2b0
LexRinn <lexrinn@outlook.com>
2023-05-07 16:07:33 +0500

    add Example 012 Methods, работа с текстом, sorting

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -4,7 +4,7 @@ void Method1()                          // В скобочках не никак
 {
     Console.WriteLine ("Autor ...");    // Тело метода. Что-то может показывать на экран.
 }
-Method1();                              // Вариант как вызывается подобного рода методы. 
+//Method1();                              // Вариант как вызывается подобного рода методы. 
                                         // Указать идентификатор метода и круглые скобки.
                                             
 // Вид 2    Ничего не возвращают, но могут принимать аргументы.
@@ -13,7 +13,7 @@ void Method2(string msg)               // Идентификатор и како
 {
     Console.WriteLine(msg);
 }
-Method2("Текст сообщения");
+//Method2("Текст сообщения");
 
 void Method21(string msg, int count)               // Идентификатор и какое-то количество аргументов.
 {
@@ -24,7 +24,7 @@ void Method21(string msg, int count)               // Идентификатор
         i++;
     }
 }
-Method21(msg: "Текст", count: 4);                   // Если использовать именованные аргументы, 
+//Method21(msg: "Текст", count: 4);                   // Если использовать именованные аргументы, 
                                                     // не обязательно писать их по порядку.
 
 
@@ -34,8 +34,8 @@ int Method3()
 {
     return DateTime.Now.Year;
 }
-int year = Method3();
-Console.WriteLine(year);
+//int year = Method3();
+//Console.WriteLine(year);
 
 // Вид 4    Что-то принимают и что-то возвращают.
 
@@ -51,8 +51,8 @@ string Method4(int count, string text)
     }
     return result;
 }
-string res = Method4(10, "z");
-Console.WriteLine(res);
+//string res = Method4(10, "z");
+//Console.WriteLine(res);
 
 // Вид 4 с использованием цикла for.    Что-то принимают и что-то возвращают.
 
@@ -65,16 +65,89 @@ string Method41(int count, string text)
     }
     return result;
 }
-string rslt = Method41(10, "asdf");
-Console.WriteLine(rslt);
+//string rslt = Method41(10, "asdf");
+//Console.WriteLine(rslt);
 
 // Цикл в цикле
 
-for (int i=2; i<=10; i++)
+// for (int i=2; i<=10; i++)
+// {
+//     for (int j=2; j<=10; j++)
+//     {
+//         Console.WriteLine($"{i}x{j}={i*j}");
+//     }
+//     Console.WriteLine();
+// }
+
+//=====Работа с текстом
+// Дан текст. В тексте нужно все пробелы заменить чёрточками,
+// маленькие буквы “к” заменить большими “К”,
+// а большие “С” маленькими “с”.
+// Ясна ли задача?
+
+string text = "— Я думаю, — сказал князь, улыбаясь, — что,"
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля."
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = "qwerty"
+//               012 
+// s[3] // r
+
+string Replace(string text, char oldValue, char newValue)           // char - конкретный символ.
 {
-    for (int j=2; j<=10; j++)
+    string result = String.Empty;                                   // Инициализация пустой строки - "result" присвоить String.Empty (пустую строку).
+
+    int lenght=text.Length;                                         // Выдаст длину текста.
+    for (int i=0; i<lenght; i++)                                    // Пробегаемся от нулевого символа до конца,
     {
-        Console.WriteLine($"{i}x{j}={i*j}");
+        if(text[i] == oldValue) result = result + $"{newValue}";    // делая эти действия.    
+        else result = result + $"{text[i]}";                        // {text[i]} - текущий символ.
+    }
+    return result;
+}
+// string newText = Replace(text, ' ', '|');
+
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// newText = Replace(newText, 'к', 'К');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// newText = Replace(newText, 'С', 'с');
+// Console.WriteLine(newText);
+
+// Сортировка.
+
+int[] arr={1, 3, 5, 3, 7, 4, 8, 1};
+
+void PrintArray(int[] array)                                        // Назвали метод PrintArray
+{
+    int count = array.Length;
+    
+    for (int i=0; i<count; i++)
+    {
+        Console.Write($"{array[i]} ");
     }
     Console.WriteLine();
 }
+
+void SelectionSort(int[] array)                                    // Назвали метод SelectionSort. В качестве аргумента будет приходить некий массив.
+{
+    for (int i=0; i<array.Length-1; i++)
+    {
+        int minPosition = i;
+
+        for (int j=i+1; j<array.Length; j++)
+        {
+            if(array[j]<array[minPosition]) minPosition=j; 
+        }
+
+        int temporary = array[i];                               // Обмен мест переменных.
+        array[i] = array [minPosition];
+        array [minPosition] = temporary;
+    }
+}
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
